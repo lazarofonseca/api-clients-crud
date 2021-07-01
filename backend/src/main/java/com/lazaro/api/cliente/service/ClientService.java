ef66b689e84906aae4b94d809184d5852207a4c7
lazarofonseca <lazaronobrega3@gmail.com>
2021-07-01 07:20:26 -0300

    findById

--- backend/src/main/java/com/lazaro/api/cliente/service/ClientService.java
+++ backend/src/main/java/com/lazaro/api/cliente/service/ClientService.java
@@ -1,5 +1,7 @@
 package com.lazaro.api.cliente.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,10 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lazaro.api.cliente.dto.ClientDTO;
 import com.lazaro.api.cliente.entities.Client;
 import com.lazaro.api.cliente.repositories.ClientReporitory;
+import com.lazaro.api.cliente.service.exception.ResourceNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	ClientReporitory clientRepository;
 
@@ -21,7 +24,13 @@ public class ClientService {
 		Page<Client> list = clientRepository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
 	}
-	
-	
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = clientRepository.findById(id);
+		Client client = obj.orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
+		return new ClientDTO(client);
+
+	}
 
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lázaro Fonseca', '000.333.555-90', 1000.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lázaro Nóbrega Fonseca', '000.333.555-90', 1000.0, TIMESTAMP WITH TIME ZONE '1986-04-13T20:50:07.12345Z', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria José Andrade', '460.973.536-78', 1800.0, TIMESTAMP WITH TIME ZONE '1980-03-13T20:50:07.12345Z', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Olimpio', '467.796.104-01', 3500.0, TIMESTAMP WITH TIME ZONE '1970-10-13T20:50:07.12345Z', 0);
