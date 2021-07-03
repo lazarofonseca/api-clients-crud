6a527f53ddd7d212a44b4ab026c23a9c7048d50b
lazarofonseca <lazaronobrega3@gmail.com>
2021-07-02 23:36:55 -0300

    Update and Delete Clients

--- backend/src/main/java/com/lazaro/api/cliente/service/ClientService.java
+++ backend/src/main/java/com/lazaro/api/cliente/service/ClientService.java
@@ -2,7 +2,11 @@ package com.lazaro.api.cliente.service;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -41,9 +45,36 @@ public class ClientService {
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
 		entity = clientRepository.save(entity);
-		
+
 		return new ClientDTO(entity);
 
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = clientRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = clientRepository.save(entity);
+
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			clientRepository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Integridade violation");
+		}
 	}
 
 }
